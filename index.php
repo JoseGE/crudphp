b81bb5a99fa5dbcc7c56595f52db93b331b11b96
JoseG <joseemzt@gmail.com>
2016-09-11 04:51:07 -0400

    proyecto CRUP PHP

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html5>
+<html>
+<head>
+	<title>Registro de Balance</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="container-fluid ">
+		<div class="container">
+			<h2>Registro de Balance</h2>
+			<form action="procesa.php"  method="POST" id="registro">
+				<div class="row">
+					<div class="col col-sm-4">
+						<div class="form-group input-group">
+						 <span class="input-group-addon">Codigo</span>
+							<input type="number" readonly value="<?php echo (isset($_GET['codigo'])) ? base64_decode($_GET['codigo']) : "";?>" class="form-control" name="codigo"/>
+						</div>
+						<div class="form-group input-group">
+							<span class="input-group-addon ">Nombre</span>
+							<input type="text" required="required" class="form-control" value="<?php echo (isset($_GET['nombre'])) ? base64_decode($_GET['nombre']) : "";?>"  name="nombre"/>
+						</div>
+						<div class="form-group input-group">
+							<span class="input-group-addon">Placa</span>
+							<input type="text" required="required" class="form-control" value="<?php echo (isset($_GET['placa'])) ? base64_decode($_GET['placa']) : "";?>"  name="placa"/>
+						</div>
+						<div class="form-group input-group">
+							<span class="input-group-addon">Balance</span>
+							<input type="number" required="required" class="form-control" value="<?php echo (isset($_GET['balance'])) ? base64_decode($_GET['balance']) : "";?>" name="balance"/>
+						</div>
+						<div class="form-group input-group">
+							<span class="input-group-addon">Comentario</span>
+							<input type="text" required="required" class="form-control" value="<?php echo (isset($_GET['comentario'])) ? base64_decode($_GET['comentario']) : "";?>" name="comentario"/>
+						</div>
+					</div>
+					<div class="col-sm-4">
+							<a class="btn btn-success" href="index.php">Nuevo</a>
+							<button class="btn btn-primary" type="submit" >Guardar</button>
+					</div>
+				</div>
+			</form>
+			
+			<div class="row">
+				<table class="table table-bordered">
+					<thead>
+						<tr>
+							<th>Codigo</th>
+							<th>Nombre</th>
+							<th>Placa</th>
+							<th>Balance</th>
+							<th>Comentario</th>
+							<th>Accion</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						//variable $files contiene arreglo de archivos en carpeta registro
+							$files = scandir("registro");
+							//se realiza un recorrido de de los archivos 
+							foreach($files as $registro) {
+								$path = "registro/{$registro}";
+								if(is_file($path)) { /*si el archivo existe se obtiene la informacion y se decodifica el json*/
+									$datos = json_decode(file_get_contents($path));
+									echo "<tr>";
+									foreach ($datos as $campo){/*se recorre cada posicion del arreglo y se asigna a su celda*/
+										echo "<td>{$campo}</td>";
+									}
+									echo "
+										<td>
+											<a href='procesa.php?modificar={$registro}' class='btn btn-warning'>Modificar</a>
+											
+											<a href='procesa.php?eliminar={$registro}' onclick='return confirm(\"Desea elminar el registro de $datos->nombre (codigo: $datos->codigo)?\")' class='btn btn-danger'>Eliminar</a>
+										</td>
+									</tr>";//enlace eliminar utiliza el objeto datos para mostrar informacion sobre el registro que se va a eliminar
+								}
+							}
+						?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ procesa.php
@@ -0,0 +1,39 @@
+<?php
+$info = array();
+	if($_POST) {
+		//crea archivo de configuracion para contador de ultimo archivo que se crea
+		$file = "conf.dat";
+		$cod = $_POST['codigo'];
+		if($cod < 1){
+			if(is_file($file)) {
+				//en caso de existir el archivo lo deserializa y aumenta el codigo
+				$conf = unserialize(file_get_contents($file));
+				$conf->cod++;
+			} else {
+				//de lo contrario se va a crear un objeto de la clase stdClass y cod sera = 1
+				$conf = new stdClass();
+				$conf->cod = 1;
+				
+			}
+		//casos que siempre sucedera para que al iniciar el programa se traigan todos los datos creados
+			file_put_contents($file,serialize($conf));
+			$cod = $conf->cod;
+		}
+		$_POST['codigo'] = $cod;
+		$registro = json_encode($_POST);
+		file_put_contents("registro/reg{$cod}.json",$registro);
+		
+		//Caso para la modificacion de registro
+	} else if(isset($_GET['modificar'])) {
+		$registro = "registro/{$_GET['modificar']}";
+		$info = json_decode(file_get_contents($registro),true);
+		header("location:index.php?codigo=".base64_encode($info['codigo'])."&nombre=".base64_encode($info['nombre'])."&placa=".base64_encode($info['placa'])."&balance=".base64_encode($info['balance'])."&comentario=".base64_encode($info['comentario']));
+		//en caso de eliminacion
+	} else if(isset($_GET['eliminar'])) {
+		$registro = "registro/{$_GET['eliminar']}";
+		if(is_file($registro)) {//para no mostrar error pregunta si el archivo existe
+			unlink($registro);
+		}
+	}
+
+?>
